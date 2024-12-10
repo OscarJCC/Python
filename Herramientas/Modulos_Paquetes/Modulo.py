6228e6db7c290e14425cc96e3e0c0dfa30140811
OscarJCC <162955624+OscarJCC@users.noreply.github.com>
2024-12-09 23:22:21 -0600

    Add files via upload

---
+++ Herramientas/Modulos_Paquetes/Modulo.py
@@ -0,0 +1,17 @@
+
+
+#from Calculadora import multiplicacion,division
+#import Calculadora as Cal
+from Paquete1.Calculadora import suma,resta,multiplicacion,division,potencia
+from Paquete1.stats import promedio
+
+print(suma(2,2))
+print(resta(2,2))
+print(multiplicacion(2,2))
+print(division(2,2))
+print(potencia(5,3))
+
+
+lista = [1,2,3,4,5,6]
+
+print(promedio(lista))
---
+++ Herramientas/Modulos_Paquetes/Modulo_libreria_estandar.py
@@ -0,0 +1,15 @@
+#Permite el uso de operaciones como el sin o cos
+"""import math as m
+print(m.pi)
+print(m.e)
+print(m.cos(m.pi/2))
+"""
+from math import *
+print(pi)
+print(e)
+print(round(cos(pi/2),2))
+print(exp(2))
+
+import random as ra
+
+print(ra.random())
